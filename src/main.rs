00ffe205864e11d4787e29095c8919f66a26cc7d
Arvid Jonasson <jonassonarvid02@gmail.com>
2023-11-03 18:18:02 +0100

    abc123

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::fs;
+use std::io::{self, ErrorKind};
+
+#[repr(u8)]
+pub enum Instruction {
+    Halt = 0,
+}
 fn main() {
-    println!("Hello, world!");
+    let file_path = "";
+    let prog = fs::read(file_path)?;
+    let mut counter: usize = 0;
+    let mut reg = [0u8; 8];
+    loop {
+        let instruction= prog[counter];
+        match instruction {
+            _ => {}
+        }
+    }
 }
