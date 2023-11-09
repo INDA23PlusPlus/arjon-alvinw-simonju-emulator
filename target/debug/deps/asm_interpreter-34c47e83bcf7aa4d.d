90c743d32eeb8ade7e0d200f23ea58cd6d281377
Simon Jutwreten <simonju@kth.se>
2023-11-09 12:54:48 +0100

    Add instructions

--- target/debug/deps/asm_interpreter-34c47e83bcf7aa4d.d
+++
@@ -1,7 +0,0 @@
-/home/sj/kth/inda/arjon-alvinw-simonju-emulator/target/debug/deps/asm_interpreter-34c47e83bcf7aa4d.rmeta: src/main.rs src/instruction_parser.rs src/instructions.rs
-
-/home/sj/kth/inda/arjon-alvinw-simonju-emulator/target/debug/deps/asm_interpreter-34c47e83bcf7aa4d.d: src/main.rs src/instruction_parser.rs src/instructions.rs
-
-src/main.rs:
-src/instruction_parser.rs:
-src/instructions.rs:
